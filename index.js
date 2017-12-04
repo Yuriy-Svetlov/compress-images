bee5da6347ac934b0fde9a1aca93df67eb01aed2
semiromid <startpascal@mail.ru>
2017-12-04 05:00:51 +0200

    1.1.5

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ var index = function (input, output, option, findfileop, enginejpg, enginepng, e
 
 
 
-    var filename, path_in_part, test_7;
+    var filename, path_in_part, test_8;
     /*
       path_in_part - путь типа - "src/img/", путь с звёздочками обрезается
     */
