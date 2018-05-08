e12785dcb14be90ffe6534dfdfae98fa13053642
semiromid <startpascal@mail.ru>
2018-05-08 14:40:56 +0300

    1.2.1

---
+++ example/compress_images.js
@@ -0,0 +1,11 @@
+    var compress_images = require('compress-images');
+
+    const INPUT_path_to_your_images = 'src/img/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}';
+    const OUTPUT_path = 'build/img/';
+    
+    compress_images(INPUT_path_to_your_images, OUTPUT_path, {compress_force: false, statistic: true, autoupdate: false}, false,
+                                                {jpg: {engine: 'mozjpeg', command: ['-quality', '60']}},
+                                                {png: {engine: 'pngquant', command: ['--quality=20-50']}},
+                                                {svg: {engine: 'svgo', command: '--multipass'}},
+                                                {gif: {engine: 'gifsicle', command: ['--colors', '64', '--use-col=web']}}, function(){
+    });
