dadfb54a3c547d6e8f50b9651ba4b8900fd2d0c7
semiromid <startpascal@mail.ru>
2018-05-09 06:47:00 +0300

    1.2.5

--- example/compress_images.js
+++ example/compress_images.js
@@ -3,7 +3,7 @@
     const INPUT_path_to_your_images = 'src/img/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}';
     const OUTPUT_path = 'build/img/';
     
-    compress_images(INPUT_path_to_your_images, OUTPUT_path, {compress_force: false, statistic: true, autoupdate: false}, false,
+    compress_images(INPUT_path_to_your_images, OUTPUT_path, {compress_force: false, statistic: true, autoupdate: true}, false,
                                                 {jpg: {engine: 'mozjpeg', command: ['-quality', '60']}},
                                                 {png: {engine: 'pngquant', command: ['--quality=20-50']}},
                                                 {svg: {engine: 'svgo', command: '--multipass'}},
--- example/minify_Img.bat
+++ example/minify_Img.bat
@@ -2,7 +2,7 @@
 @echo off
 node compress_images.js 
 @echo 'Compress images completed'
-@echo 'Console will be close after [20] seconds'
+@echo 'Console will be close after [20] seconds...'
 @echo off 
 ping 127.0.0.1 -n 20 > nul
 
