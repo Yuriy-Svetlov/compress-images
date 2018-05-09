b43315f62a174665ee1d029722b91d5286610f8f
semiromid <startpascal@mail.ru>
2018-05-09 06:19:38 +0300

    1.2.3

--- example/minify_Img.bat
+++ example/minify_Img.bat
@@ -1,1 +1,9 @@
-node compress_images.js
+@echo 'Starting compress images'
+@echo off
+node compress_images.js 
+@echo 'Compress images completed'
+@echo 'Console will be close after [20] seconds'
+@echo off 
+ping 127.0.0.1 -n 20 > nul
+
+
