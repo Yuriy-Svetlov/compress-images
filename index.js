588396fa46eb081639a31d5451883dff2425481e
semiromid <startpascal@mail.ru>
2018-10-18 13:40:33 +0300

    1.4.3

--- index.js
+++ index.js
@@ -1501,7 +1501,7 @@ var index = function (input, output, option, findfileop, enginejpg, enginepng, e
       checkUpdate();
 
       if(err !== null){
-        var dataError = {}
+        var dataError = {};
         if(length_files === 0){
           dataError.engine = engine;
           dataError.input = input;
